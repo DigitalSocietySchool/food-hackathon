bca6c84d93f9b9904d0c240d1ae567bab184de1c
iKettles <ikettles@gmail.com>
2014-10-18 02:13:02 +0200

    commit
    aa

--- FoodToGo/src/com/medialab/foodtogo/TestActivity.java
+++ FoodToGo/src/com/medialab/foodtogo/TestActivity.java
@@ -14,9 +14,11 @@ public class TestActivity extends ActionBarActivity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) { 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.test, menu);
+		getMenuInflater().inflate(R.menu.test, menu); 
+		
+		
 		return true;
 	}
 
